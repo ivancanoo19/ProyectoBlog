b9e0f491af60bf41dcd1a7bd34ad77bed3d58e64
ivancanoo19 <143280189+ivancanoo19@users.noreply.github.com>
2023-11-09 11:16:29 -0600

    [MOD] Agregando rutas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +19,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+*/
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    // Rutas para administradores
+    Route::get('/admin_home', [HomeController::class, 'adminHome'])->name('admin.home');
+
+    // Rutas para usuarios normales
+    Route::get('/user_home', [HomeController::class, 'userHome'])->name('user.home');
 });
 
 require __DIR__.'/auth.php';
