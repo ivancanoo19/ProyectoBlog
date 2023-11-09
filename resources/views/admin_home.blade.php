660557a9cf20b5dfad16d07fd5647fd2b6c379c7
ivancanoo19 <143280189+ivancanoo19@users.noreply.github.com>
2023-11-09 11:15:21 -0600

    [ADD] Vistas de admin y user

---
+++ resources/views/admin_home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Home</title>
+</head>
+<body>
+    <h1>Bienvenido, {{ Auth::user()->name }} (Admin)</h1>
+    <p>Has iniciado sesión como administrador correctamente.</p>
+</body>
+</html>
