6c52046558fce7b747d4b80def2272d4078a8fd0
ivancanoo19 <143280189+ivancanoo19@users.noreply.github.com>
2023-11-09 11:14:55 -0600

    [FEATURE] Cargando admins a la base

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;    //Para poder inertar datos al modelo de User
+use Illuminate\Support\Facades\Hash;    //Para hacer un mapeo Hash a las contraseñas
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,29 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        /*
+        
+        Ejecutar solamente una vez el seeder para no tener
+        usuarios administradores repetidos...
+        
+        php artisan db:seed
+        */
+
+
+        User::create([
+            'name' => 'cano',
+            'email' => 'cano123@gmail.com',
+            'password' => Hash::make('123456789'),
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'nat',
+            'email' => 'nat123@gmail.com',
+            'password' => Hash::make('123456789'),
+            'role' => 'admin',
+        ]);
     }
 }
